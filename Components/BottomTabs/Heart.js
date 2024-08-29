41c23114ba1b9f017eca2d3bc740a67a59203b92
Amoh-Ntim <amohntim123@gmail.com>
2024-08-29 18:24:18 +0000

    Faith to finish kamui kidney done. left with shap values

--- Components/BottomTabs/Heart.js
+++ Components/BottomTabs/Heart.js
@@ -1,12 +1,13 @@
-import React, { useState,useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { View, ScrollView, Text, TextInput, Button, FlatList } from 'react-native';
 import tw from 'twrnc';
-import DropDownPicker from 'react-native-dropdown-picker';
-import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import { Dropdown } from 'react-native-element-dropdown';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import Donut from './Donut';
 import Mybarchart from './Mybarchart';
+import axios from 'axios';
 
-const InputForm = () => {
+const Heart = () => {
   const [age, setAge] = useState('');
   const [sex, setSex] = useState(null);
   const [chestPainType, setChestPainType] = useState(null);
@@ -21,64 +22,96 @@ const InputForm = () => {
   const [majorVessels, setMajorVessels] = useState('');
   const [thalassemia, setThalassemia] = useState(null);
 
-  const [openSex, setOpenSex] = useState(false);
-  const [sexItems, setSexItems] = useState([
+  const [prediction, setPrediction] = useState(null);
+  const [probability, setProbability] = useState(null);
+  const [error, setError] = useState(null);
+
+  const sexItems = [
     { label: 'Male', value: 1 },
     { label: 'Female', value: 0 },
-  ]);
+  ];
 
-  const [openChestPain, setOpenChestPain] = useState(false);
-  const [chestPainItems, setChestPainItems] = useState([
+  const chestPainItems = [
     { label: 'Typical Angina', value: 0 },
     { label: 'Atypical Angina', value: 1 },
     { label: 'Non-Anginal Pain', value: 2 },
-    { label: 'Asymptotic', value: 3},
-  ]);
+    { label: 'Asymptotic', value: 3 },
+  ];
 
-  const [openFastingBloodSugar, setOpenFastingBloodSugar] = useState(false);
-  const [fastingBloodSugarItems, setFastingBloodSugarItems] = useState([
+  const fastingBloodSugarItems = [
     { label: 'False', value: 0 },
     { label: 'True', value: 1 },
-  ]);
+  ];
 
-  const [openExerciseInducedAngina, setOpenExerciseInducedAngina] = useState(false);
-  const [exerciseInducedAnginaItems, setExerciseInducedAnginaItems] = useState([
+  const exerciseInducedAnginaItems = [
     { label: 'No', value: 0 },
     { label: 'Yes', value: 1 },
-  ]);
+  ];
 
-  const [openStSlope, setOpenStSlope] = useState(false);
-  const [stSlopeItems, setStSlopeItems] = useState([
+  const stSlopeItems = [
     { label: 'Upsloping', value: 0 },
     { label: 'Flat', value: 1 },
     { label: 'Downsloping', value: 2 },
-  ]);
+  ];
 
-  const [openThalassemia, setOpenThalassemia] = useState(false);
-  const [thalassemiaItems, setThalassemiaItems] = useState([
+  const thalassemiaItems = [
     { label: 'Normal', value: 0 },
     { label: 'Fixed Defect', value: 1 },
     { label: 'Reversible Defect', value: 2 },
-  ]);
+  ];
 
   const formFields = [
     { key: 'age', label: 'Age', value: age, setter: setAge, keyboardType: 'numeric', type: 'text' },
-    { key: 'sex', label: 'Sex', value: sex, setter: setSex, items: sexItems, open: openSex, setOpen: setOpenSex, type: 'dropdown' },
-    { key: 'chestPainType', label: 'Chest Pain Type', value: chestPainType, setter: setChestPainType, items: chestPainItems, open: openChestPain, setOpen: setOpenChestPain, type: 'dropdown' },
+    { key: 'sex', label: 'Sex', value: sex, setter: setSex, items: sexItems, type: 'dropdown' },
+    { key: 'chestPainType', label: 'Chest Pain Type', value: chestPainType, setter: setChestPainType, items: chestPainItems, type: 'dropdown' },
     { key: 'restingBP', label: 'Resting Blood Pressure', value: restingBP, setter: setRestingBP, keyboardType: 'numeric', type: 'text' },
     { key: 'serumCholesterol', label: 'Serum Cholesterol', value: serumCholesterol, setter: setSerumCholesterol, keyboardType: 'numeric', type: 'text' },
-    { key: 'fastingBloodSugar', label: 'Fasting Blood Sugar', value: fastingBloodSugar, setter: setFastingBloodSugar, items: fastingBloodSugarItems, open: openFastingBloodSugar, setOpen: setOpenFastingBloodSugar, type: 'dropdown' },
+    { key: 'fastingBloodSugar', label: 'Fasting Blood Sugar', value: fastingBloodSugar, setter: setFastingBloodSugar, items: fastingBloodSugarItems, type: 'dropdown' },
     { key: 'restingECG', label: 'Resting ECG', value: restingECG, setter: setRestingECG, keyboardType: 'numeric', type: 'text' },
     { key: 'maxHeartRate', label: 'Max Heart Rate', value: maxHeartRate, setter: setMaxHeartRate, keyboardType: 'numeric', type: 'text' },
-    { key: 'exerciseInducedAngina', label: 'Exercise Induced Angina', value: exerciseInducedAngina, setter: setExerciseInducedAngina, items: exerciseInducedAnginaItems, open: openExerciseInducedAngina, setOpen: setOpenExerciseInducedAngina, type: 'dropdown' },
+    { key: 'exerciseInducedAngina', label: 'Exercise Induced Angina', value: exerciseInducedAngina, setter: setExerciseInducedAngina, items: exerciseInducedAnginaItems, type: 'dropdown' },
     { key: 'stDepression', label: 'ST Depression', value: stDepression, setter: setStDepression, keyboardType: 'numeric', type: 'text' },
-    { key: 'stSlope', label: 'Slope of the Peak Exercise ST Segment', value: stSlope, setter: setStSlope, items: stSlopeItems, open: openStSlope, setOpen: setOpenStSlope, type: 'dropdown' },
+    { key: 'stSlope', label: 'Slope of the Peak Exercise ST Segment', value: stSlope, setter: setStSlope, items: stSlopeItems, type: 'dropdown' },
     { key: 'majorVessels', label: 'Number of Major Vessels Colored by Fluoroscopy', value: majorVessels, setter: setMajorVessels, keyboardType: 'numeric', type: 'text' },
-    { key: 'thalassemia', label: 'Thalassemia', value: thalassemia, setter: setThalassemia, items: thalassemiaItems, open: openThalassemia, setOpen: setOpenThalassemia, type: 'dropdown' },
+    { key: 'thalassemia', label: 'Thalassemia', value: thalassemia, setter: setThalassemia, items: thalassemiaItems, type: 'dropdown' },
   ];
+
   const bottomSheetRef = useRef(null);
+
+  const handlePredictPress = async () => {
+    const dataToSend = {
+      age: age,
+      sex: sex,
+      cp: chestPainType,
+      trestbps: restingBP,
+      chol: serumCholesterol,
+      fbs: fastingBloodSugar,
+      restecg: restingECG,
+      thalach: maxHeartRate,
+      exang: exerciseInducedAngina,
+      oldpeak: stDepression,
+      slope: stSlope,
+      ca: majorVessels,
+      thal: thalassemia,
+    };
+
+    console.log('Data to send:', dataToSend);
+
+    try {
+      const response = await axios.post('http://192.168.78.69:5000/api/predict_heart_disease', dataToSend);
+
+      setPrediction(response.data.predictions);
+      setProbability(response.data.probabilities * 100);
+    } catch (error) {
+      console.error('Error making prediction:', error);
+      setError('There was an error making the prediction. Please try again later.');
+    }
+
+    handlePresentSheetPress();
+  };
+
   const handlePresentSheetPress = () => {
-    if (prediction || error) { // Check if prediction or error exists
+    if (prediction || error) {
       bottomSheetRef.current.expand();
     }
   };
@@ -88,16 +121,15 @@ const InputForm = () => {
       return (
         <View style={tw`mb-4`}>
           <Text style={tw`text-black mb-2`}>{item.label}</Text>
-          <DropDownPicker
-            open={item.open}
-            value={item.value}
-            items={item.items}
-            setOpen={item.setOpen}
-            setValue={item.setter}
+          <Dropdown
+            data={item.items}
+            labelField="label"
+            valueField="value"
             placeholder={`Select ${item.label}`}
+            value={item.value}
+            onChange={item.setter}
             containerStyle={tw`border border-gray-300`}
-            style={tw`z-50 elevation-50 border border-gray-300`}
-            dropDownStyle={tw`border border-gray-300`}
+            style={tw`border border-gray-300 p-2 rounded`}
           />
         </View>
       );
@@ -107,69 +139,50 @@ const InputForm = () => {
           <Text style={tw`text-black mb-2`}>{item.label}</Text>
           <TextInput
             keyboardType={item.keyboardType}
-            placeholder={item.label}
             value={item.value}
             onChangeText={item.setter}
-            style={tw`border border-gray-300 p-2`}
+            placeholder={item.label}
+            style={tw`border border-gray-300 p-2 rounded`}
           />
         </View>
       );
     }
   };
 
-  const handlePredictPress = () => {
-    // Handle prediction logic here
-    console.log('Predict button pressed');
-    console.log({
-      age,
-      sex,
-      chestPainType,
-      restingBP,
-      serumCholesterol,
-      fastingBloodSugar,
-      restingECG,
-      maxHeartRate,
-      exerciseInducedAngina,
-      stDepression,
-      stSlope,
-      majorVessels,
-      thalassemia,
-    });
-    
-  };
-
   return (
-    <>
+    <View style={tw`flex-1 justify-center p-4`}>
+      <ScrollView style={tw`mb-4`}>
+        <FlatList
+          data={formFields}
+          renderItem={renderItem}
+          keyExtractor={item => item.key}
+          scrollEnabled={false}
+        />
+      </ScrollView>
 
-    <View style={tw`flex-1 p-4`}>
-      <FlatList
-        data={formFields}
-        renderItem={renderItem}
-        keyExtractor={item => item.key}
-        contentContainerStyle={tw`pb-4`}
+      <Button
+        title="Predict"
+        onPress={handlePredictPress}
       />
-    </View>
-      <Button title="Predict" onPress={handlePredictPress} />
+
       <BottomSheet
-           ref={bottomSheetRef}
-           index={0}
-           snapPoints={['25%', '50%' ,'80%', ]}
-           enablePanDownToClose={true}
-           >
-           <BottomSheetView>
-           {prediction ? <Text>Prediction: {prediction}</Text> : null}
-           {probability ? <Text>Probability: {probability}</Text> : null}
-           {error ? <Text style={tw`text-red-500`}>Error: {error}</Text> : null}
-           <View style={tw`flex justify-center items-center`}>
-            <Donut percentage={probability} color="tomato" max={100} />
-           </View>
-           <View style={tw`flex justify-center items-center`}>
-            <Mybarchart/>
-           </View>
-           </BottomSheetView>
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={['25%', '50%', '75%']}
+      >
+        <BottomSheetView style={tw`p-4`}>
+          {error ? (
+            <Text style={tw`text-red-500`}>{error}</Text>
+          ) : (
+            <View>
+              <Donut percentage={probability} />
+              <Mybarchart />
+            </View>
+          )}
+        </BottomSheetView>
       </BottomSheet>
-    </>
+    </View>
   );
 };
 
-export default InputForm;
+export default Heart;
--- Components/BottomTabs/Liver.js
+++ Components/BottomTabs/Liver.js
@@ -1,165 +1,193 @@
-import React, { useState,useRef  } from 'react';
-import { View, ScrollView, Text, SafeAreaView, TextInput, Button, FlatList } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, ScrollView, Text, SafeAreaView, TextInput, Button } from 'react-native';
 import tw from 'twrnc';
-import DropDownPicker from 'react-native-dropdown-picker';
+import { Dropdown } from 'react-native-element-dropdown';
 import axios from 'axios';
-import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import Donut from './Donut';
 import Mybarchart from './Mybarchart';
 
 const Liver = () => {
-  const [openGender, setOpenGender] = useState(false);
-  const [genderValue, setGenderValue] = useState(null);
   const [age, setAge] = useState('');
   const [totalBilirubin, setTotalBilirubin] = useState('');
   const [directBilirubin, setDirectBilirubin] = useState('');
-  const [alkanePhosphatase, setAlkanePhosphatase] = useState('');
-  const [alanineAminotransferase, setAlanineAminotransferase] = useState('');
+  const [alkalinePhosphotase, setAlkalinePhosphotase] = useState('');
+  const [alamineAminotransferase, setAlamineAminotransferase] = useState('');
   const [aspartateAminotransferase, setAspartateAminotransferase] = useState('');
   const [totalProteins, setTotalProteins] = useState('');
   const [albumin, setAlbumin] = useState('');
   const [albuminGlobulinRatio, setAlbuminGlobulinRatio] = useState('');
+  const [sex, setSex] = useState(null);
   const [prediction, setPrediction] = useState('');
   const [probability, setProbability] = useState('');
-  const [error, setError] = useState('');  // Define state for error handling
+  const [error, setError] = useState('');
 
-  const [genderItems, setGenderItems] = useState([
+  const sexItems = [
     { label: 'Male', value: 1 },
     { label: 'Female', value: 0 },
-  ]);
-
-  const formFields = [
-    { key: 'age', label: 'Age', value: age, setter: setAge, keyboardType: 'numeric', type: 'text' },
-    { key: 'gender', label: 'Gender', value: genderValue, setter: setGenderValue, items: genderItems, open: openGender, setOpen: setOpenGender, type: 'dropdown' },
-    { key: 'totalBilirubin', label: 'Total Bilirubin', value: totalBilirubin, setter: setTotalBilirubin, keyboardType: 'decimal-pad', type: 'text' },
-    { key: 'directBilirubin', label: 'Direct Bilirubin', value: directBilirubin, setter: setDirectBilirubin, keyboardType: 'decimal-pad', type: 'text' },
-    { key: 'alkanePhosphatase', label: 'Alkane Phosphatase', value: alkanePhosphatase, setter: setAlkanePhosphatase, keyboardType: 'numeric', type: 'text' },
-    { key: 'alanineAminotransferase', label: 'Alanine Aminotransferase', value: alanineAminotransferase, setter: setAlanineAminotransferase, keyboardType: 'numeric', type: 'text' },
-    { key: 'aspartateAminotransferase', label: 'Aspartate Aminotransferase', value: aspartateAminotransferase, setter: setAspartateAminotransferase, keyboardType: 'numeric', type: 'text' },
-    { key: 'totalProteins', label: 'Total Proteins', value: totalProteins, setter: setTotalProteins, keyboardType: 'decimal-pad', type: 'text' },
-    { key: 'albumin', label: 'Albumin', value: albumin, setter: setAlbumin, keyboardType: 'decimal-pad', type: 'text' },
-    { key: 'albuminGlobulinRatio', label: 'Albumin and Globulin Ratio', value: albuminGlobulinRatio, setter: setAlbuminGlobulinRatio, keyboardType: 'decimal-pad', type: 'text' },
   ];
 
-  const renderItem = ({ item }) => {
-    if (item.type === 'dropdown') {
-      return (
-        <View style={tw`mb-4`}>
-          <Text style={tw`text-black mb-2`}>{item.label}</Text>
-          <DropDownPicker
-            open={item.open}
-            value={item.value}
-            items={item.items}
-            setOpen={item.setOpen}
-            setValue={item.setter}
-            placeholder={`Select ${item.label}`}
-            containerStyle={tw`border border-gray-300`}
-            style={tw`z-50 elevation-50 border border-gray-300`}
-            dropDownStyle={tw`border border-gray-300`}
-            // style={{ backgroundColor: '#fafafa' }}
-        dropDownContainerStyle={{
-          backgroundColor: '#fafafa',
-          zIndex: 1000, // Ensure the dropdown appears above other components
-          elevation: 1000, // For Android
-        }}
-        zIndex={1000} // Ensure the dropdown appears above other components
-        zIndexInverse={3000} // Ensure the dropdown appears above other components
-          />
-        </View>
-      );
-    } else {
-      return (
-        <View style={tw`mb-4`}>
-          <Text style={tw`text-black mb-2`}>{item.label}</Text>
-          <TextInput
-            keyboardType={item.keyboardType}
-            placeholder={`Enter ${item.label}`}
-            value={item.value}
-            onChangeText={item.setter}
-            style={tw`border border-gray-300 p-2`}
-          />
-        </View>
-      );
-    }
-  };
-
-  const handlePredictPress = async () => {
-    const requestBody = {
-      Age: age,  // Assuming 'age' is a variable in your component
-  Gender: genderValue,  // Assuming 'genderValue' is defined somewhere in your component
-  Total_Bilirubin: totalBilirubin,  // Assuming 'totalBilirubin' is a state or variable
-  Direct_Bilirubin: directBilirubin,  // Assuming 'directBilirubin' is a state or variable
-  Alkaline_Phosphotase: alkanePhosphatase,  // Assuming 'alkanePhosphatase' is a state or variable
-  Alamine_Aminotransferase: alanineAminotransferase,  // Assuming 'alanineAminotransferase' is a state or variable
-  Aspartate_Aminotransferase: aspartateAminotransferase,  // Assuming 'aspartateAminotransferase' is a state or variable
-  Total_Protiens: totalProteins,  // Assuming 'totalProteins' is a state or variable
-  Albumin: albumin,  // Assuming 'albumin' is a state or variable
-  Albumin_and_Globulin_Ratio: albuminGlobulinRatio,  // Assuming 'albuminGlobulinRatio' is a state or variable
-
-    };
-
-    console.log('Request Body:', requestBody);  // Log the request body
-
-    try {
-      const response = await axios.post('http://192.168.202.69:5000/api/predict_liver_disease', requestBody);
-
-      setPrediction(response.data.predicted_class);
-      setProbability(response.data.probability);
-      console.log('API Response:', response.data);
-      setError('');  // Clear any previous errors
-    } catch (error) {
-      console.error('Error object:', error);
-      console.error('Error response:', error.response);
-      console.error('Error:', error);
-      setError(error.response?.data?.error || error.message);
-      Alert.alert('Error', error.response?.data?.error || error.message);
-    }
-  };
   const bottomSheetRef = useRef(null);
   const handlePresentSheetPress = () => {
-    if (prediction || error) { // Check if prediction or error exists
+    if (prediction || error) {
       bottomSheetRef.current.expand();
     }
   };
 
+  const handlePredictPress = async () => {
+    const dataToSend = {
+      Age: age,
+      Total_Bilirubin: totalBilirubin,
+      Direct_Bilirubin: directBilirubin,
+      Alkaline_Phosphotase: alkalinePhosphotase,
+      Alamine_Aminotransferase: alamineAminotransferase,
+      Aspartate_Aminotransferase: aspartateAminotransferase,
+      Total_Proteins: totalProteins,
+      Albumin: albumin,
+      Albumin_and_Globulin_Ratio: albuminGlobulinRatio,
+      Gender: sex,
+    };
+
+    console.log('Data to send:', dataToSend);
+
+    try {
+      const response = await axios.post('http://192.168.78.69:5000/api/predict_liver', dataToSend);
+      
+      setPrediction(response.data.predictions);
+      setProbability(response.data.probabilities * 100);
+    } catch (error) {
+      console.error('Error making prediction:', error);
+      alert('There was an error making the prediction. Please try again later.');
+    }
+  };
+
+  const handleCombinedPress = () => {
+    handlePredictPress();
+    handlePresentSheetPress();
+  };
+
   return (
-    <SafeAreaView style={tw`flex-1`}>
-      <View style={tw`flex justify-center items-center p-4`}>
-        <Text style={tw`font-bold text-xl`}>LIVER DISEASE PREDICTION</Text>
-      </View>
-      <View style={tw`flex-1 p-4`}>
-        <FlatList
-          data={formFields}
-          renderItem={renderItem}
-          keyExtractor={item => item.key}
-          contentContainerStyle={tw`pb-4`}
-        />
-      </View>
-      <View style={tw`p-4`}>
-        <Button title="Predict" onPress={() => {
-        handlePresentSheetPress();
-        handlePredictPress();
-        }} />
-      </View>
+    <>
+      <SafeAreaView>
+        <ScrollView style={tw`px-4 py-8`}>
+          <Text style={tw`text-center text-xl font-bold mb-4`}>Liver Disease Prediction</Text>
+
+          <Text style={tw`text-base mb-2`}>Age:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Age"
+            value={age}
+            onChangeText={setAge}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Total Bilirubin:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Total Bilirubin"
+            value={totalBilirubin}
+            onChangeText={setTotalBilirubin}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Direct Bilirubin:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Direct Bilirubin"
+            value={directBilirubin}
+            onChangeText={setDirectBilirubin}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Alkaline Phosphotase:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Alkaline Phosphotase"
+            value={alkalinePhosphotase}
+            onChangeText={setAlkalinePhosphotase}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Alamine Aminotransferase:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Alamine Aminotransferase"
+            value={alamineAminotransferase}
+            onChangeText={setAlamineAminotransferase}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Aspartate Aminotransferase:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Aspartate Aminotransferase"
+            value={aspartateAminotransferase}
+            onChangeText={setAspartateAminotransferase}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Total Proteins:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Total Proteins"
+            value={totalProteins}
+            onChangeText={setTotalProteins}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Albumin:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Albumin"
+            value={albumin}
+            onChangeText={setAlbumin}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Albumin and Globulin Ratio:</Text>
+          <TextInput
+            keyboardType="numeric"
+            placeholder="Albumin and Globulin Ratio"
+            value={albuminGlobulinRatio}
+            onChangeText={setAlbuminGlobulinRatio}
+            style={tw`border p-2 mb-4 border-gray-300`}
+          />
+
+          <Text style={tw`text-base mb-2`}>Sex:</Text>
+          <Dropdown
+            style={tw`border p-2 mb-4 border-gray-300`}
+            data={sexItems}
+            labelField="label"
+            valueField="value"
+            placeholder="Select Sex"
+            value={sex}
+            onChange={(value) => setSex(value.value)}
+          />
+
+          <Button title="Predict" onPress={handleCombinedPress} />
+        </ScrollView>
+      </SafeAreaView>
+
       <BottomSheet
-           ref={bottomSheetRef}
-           index={0}
-           snapPoints={['25%', '50%' ,'80%', ]}
-           enablePanDownToClose={true}
-           >
-           <BottomSheetView>
-           {prediction ? <Text>Prediction: {prediction}</Text> : null}
-           {probability ? <Text>Probability: {probability}</Text> : null}
-           {error ? <Text style={tw`text-red-500`}>Error: {error}</Text> : null}
-           <View style={tw`flex justify-center items-center`}>
+        ref={bottomSheetRef}
+        index={0}
+        snapPoints={['25%', '50%', '80%']}
+        enablePanDownToClose={true}
+      >
+        <BottomSheetView>
+          {prediction ? <Text>Prediction: {prediction}</Text> : null}
+          {probability ? <Text>Probability: {probability} %</Text> : null}
+
+          {error ? <Text style={tw`text-red-500`}>Error: {error}</Text> : null}
+          <View style={tw`flex justify-center items-center`}>
             <Donut percentage={probability} color="tomato" max={100} />
-           </View>
-           <View style={tw`flex justify-center items-center`}>
-            <Mybarchart/>
-           </View>
-           </BottomSheetView>
+          </View>
+          <View style={tw`flex justify-center items-center`}>
+            <Mybarchart />
+          </View>
+        </BottomSheetView>
       </BottomSheet>
-    </SafeAreaView>
+    </>
   );
 };
 
